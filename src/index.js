4f5abd606efdde6c48c13be27865ead175ae3a1f
ye-russell <bekberata@gmail.com>
2021-03-11 10:10:53 +0600

    feat: complete the soultion in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length != 0) return Math.min(...array);
+    else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length != 0) return Math.max(...array);
+    else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (Array.isArray(array) && array.length != 0) {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else return 0;
 }
